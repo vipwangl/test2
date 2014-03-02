561a008b82f95094f0b91bf717c82ac0c65cf5e5
vipwangl <359672270@qq.com>
2014-03-02 20:04:08 +0800

    输入说明

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeiBoDemo
+//
+//  Created by Mr Wang on 14-2-21.
+//  Copyright (c) 2014年 Mama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
